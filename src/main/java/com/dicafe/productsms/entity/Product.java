2cf2c30ae92ddd3576dcc3dd39268e16ffb41b3b
JohanRiveraC <johan0513@hotmail.com>
2021-11-12 21:08:50 -0500

    Base

--- src/main/java/com/dicafe/productsms/entity/Product.java
+++ src/main/java/com/dicafe/productsms/entity/Product.java
@@ -1,5 +1,7 @@
 package com.dicafe.productsms.entity;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,6 +9,7 @@ import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Entity
+@Data
 
 public class Product {
 
